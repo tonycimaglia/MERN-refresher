b717c8e8cd296d7df35a12945ffff5bd87751d5f
tonycimaglia <tonycimaglia@gmail.com>
2018-08-12 11:30:55 -0400

    connected to db, required dependencies, set up app to run on port 3001.

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const app = express()
+
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+
+db.on('error', err => {
+    console.log(err);
+})
+
+db.on('open', () => {
+    console.log('Connected to MongoDB')
+})
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('hello world!')
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log(`App is up and running on http://localhost:${PORT}`)
+})
