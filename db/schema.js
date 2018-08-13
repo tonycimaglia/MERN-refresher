73d871f6980310900166e1592fcbcf5dc5182a3a
tonycimaglia <tonycimaglia@gmail.com>
2018-08-13 19:38:09 -0400

    created a db directory and a schema and seeds file. Wrote out and exported the user schema.

---
+++ db/schema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    passWord: {
+        type: String,
+        required: true,
+    },
+    firstName: String,
+    lastName: String,
+})
