b2dcd11d46adc4d76efc2260dd3a8b1b4d5783b7
tonycimaglia <tonycimaglia@gmail.com>
2018-08-23 09:41:51 -0400

    imported React Router into app.js, built out routes to singleUser and all usercomponent.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import Users from './components/Users'
+// import SingleUser from './components/SingeUser'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            {/* <Route exact path="/" component={Users}/>
+            <Route exact path="/:id" component={SingleUser}/> */}
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
