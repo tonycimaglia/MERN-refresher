6b72de9ce6460255e8cb8e1072e15dab281af493
tonycimaglia <tonycimaglia@gmail.com>
2018-08-23 09:35:12 -0400

    installed axios styled-components react-router-dom

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get('/*', (req, res) => {
 //     res.send('hello world!')
 // })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => {
     console.log(`App is up and running on http://localhost:${PORT}`)
