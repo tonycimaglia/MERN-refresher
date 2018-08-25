8fe2b9ad3972a41bd675007318c08fe0915ed4a6
tonycimaglia <tonycimaglia@gmail.com>
2018-08-24 23:15:05 -0400

    created axios request to query db for all users, and stored them in users state.

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import axios from 'axios'
 // import NewUserForm from './NewUserForm'
 
 class Users extends Component {
+    state = {
+        users: [],
+        showCreateForm: false
+    }
+
+    componentWillMount () {
+        this.getAllUsers()
+    }
+
+    getAllUsers = async () => {
+        const res = await axios.get('/api/users')
+        this.setState({users: res.data})
+    }
 
     render () {
         return (
